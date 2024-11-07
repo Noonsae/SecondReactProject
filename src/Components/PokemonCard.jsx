3afbd85e46867c3fbbeb6be03996601bc4175735
Noon_sae <minseock7308@gmail.com>
2024-11-07 19:10:29 +0900

    📝mockData 읽어오기 및 추가기능 구현 진행

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,9 +1,32 @@
-
+import { useState } from "react";
+import { mockData } from "../assets/data/mockData";
 
 const PokemonCard = () => {
-  return (
-    <div>PokemonCard</div>
-  )
-}
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const handleSelectPokemon = (e) => {
+    
+    const pokemonId = e.target.id;
+    console.log(pokemonId)    
+  };
 
-export default PokemonCard
+  return (
+    <>
+      {mockData.map((data) => (
+        <div className="pokemon-card" key={data.id}>
+          <img src={data.img_url} alt={data.korean_name} />
+          <h3>{data.korean_name}</h3>
+          <p>No : {String(data.id).padStart(3, "0")}</p>
+          <button
+            type="button"
+            id={data.korean_name}
+            onClick={handleSelectPokemon}
+          >
+            추가
+          </button>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default PokemonCard;
