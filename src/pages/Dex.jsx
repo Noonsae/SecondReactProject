824cbeb90d977350d2f56210dfd939168f8b142c
Noon_sae <minseock7308@gmail.com>
2024-11-07 21:37:32 +0900

    📝컴포넌트 분리 후 props 전달

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,11 +1,35 @@
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
+import { mockData } from "../assets/data/mockData";
+import { useState } from "react";
 
 const Dex = () => {
+  const [selectedPokemon, setSelectedPokemon] = useState("");
+
+  const handleSelectPokemon = (e) => {
+    const pokemonName = e.target.id;
+    console.log(pokemonName);
+
+    const selectedPokemonData = mockData.find(
+      (pokemon) => pokemon.korean_name === pokemonName
+    );
+
+    if (selectedPokemonData) {
+      setSelectedPokemon(selectedPokemonData);
+    }
+  };
+
   return (
     <div className="dex-warp">
-      <Dashboard />
-      <PokemonList />
+      <Dashboard
+        mockData={mockData}
+        handleSelectPokemon={handleSelectPokemon}
+        selectedPokemon={selectedPokemon}
+      />
+      <PokemonList
+        mockData={mockData}
+        handleSelectPokemon={handleSelectPokemon}
+      />
     </div>
   );
 };
