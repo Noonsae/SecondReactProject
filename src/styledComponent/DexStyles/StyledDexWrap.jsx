3788aec3ca7c8da56cdd7963eff2d17e00e1869d
Noon_sae <minseock7308@gmail.com>
2024-11-08 22:32:33 +0900

    📝아이템 가운데 정렬

--- src/styledComponent/DexStyles/StyledDexWrap.jsx
+++ src/styledComponent/DexStyles/StyledDexWrap.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledDexWrap = styled.div`
   width: 100%;
   margin: 0 auto;
+  text-align: center;
 
   padding: 30px 0;
   background-color: #fff3fb;
