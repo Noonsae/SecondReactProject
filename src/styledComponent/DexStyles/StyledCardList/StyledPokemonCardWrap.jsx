7735050a66010bc37f47c4ddff05a4f0c400cdc8
Noon_sae <minseock7308@gmail.com>
2024-11-08 22:31:40 +0900

    📝대쉬보드 배경색상 및 테두리 변경

--- src/styledComponent/DexStyles/StyledCardList/StyledPokemonCardWrap.jsx
+++ src/styledComponent/DexStyles/StyledCardList/StyledPokemonCardWrap.jsx
@@ -5,8 +5,8 @@ export const StyledPokemonCardWrap = styled.div`
   margin: 0 auto;
   padding: 10px 20px 0;
 
-  background-color: #fbeaf6;
-  border-radius: 20px 0 0 0;
+  background-color:  #ffe9f2;
+  border-radius: 20px;
 
   display: flex;
   flex-direction: row;
