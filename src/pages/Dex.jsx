0569aec3f315744edc015a55d48b7f192abf580c
Noon_sae <minseock7308@gmail.com>
2024-11-08 22:32:54 +0900

    📝홈으로 돌아가는 버튼 구현 단계

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -3,6 +3,7 @@ import PokemonCardList from "../components/PokemonCardList";
 import { mockData } from "../assets/data/mockData";
 import { useState } from "react";
 import { StyledDexWrap } from "../styledComponent/DexStyles/StyledDexWrap";
+import {StyledGoToBackBtn} from "../styledComponent/DexStyles/StyledDashBoard/StyledGoToBackBtn";
 
 const Dex = () => {
   const [selectedPokemon, setSelectedPokemon] = useState("");
@@ -22,6 +23,7 @@ const Dex = () => {
 
   return (
     <StyledDexWrap>
+      <StyledGoToBackBtn>뒤로가기</StyledGoToBackBtn>
       <Dashboard
         mockData={mockData}
         handleSelectPokemon={handleSelectPokemon}
---
+++ src/styledComponent/DexStyles/StyledDashBoard/StyledGoToBackBtn.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledGoToBackBtn = styled.button`
+  display: block;
+  background-color: #fff;
+  position: absolute;
+  left: 50px;
+  top: 50px;
+`
