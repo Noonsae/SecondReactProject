a21cecfa6388f6b1f0d1d4fa139485027fe2fd8f
Noon_sae <minseock7308@gmail.com>
2024-11-08 13:29:35 +0900

    🔨 배경색상, 여백, 정렬방법조정

--- src/styledComponent/DexStyles/PokemonCardWrap.jsx
+++ src/styledComponent/DexStyles/PokemonCardWrap.jsx
@@ -3,11 +3,12 @@ import styled from "styled-components";
 export const PokemonCardWrap = styled.div`
   width: 80%;
   margin: 0 auto;
+  padding: 10px 20px 0;
 
-  background-color: #fff;
+  background-color: #fbeaf6;
 
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: space-between;  
+  justify-content: space-evenly;
 `;
