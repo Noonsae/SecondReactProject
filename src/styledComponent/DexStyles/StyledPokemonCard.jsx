bcbf553586e1943138dbd83a183c11c1a09c1217
Noon_sae <minseock7308@gmail.com>
2024-11-08 13:29:57 +0900

    📝PokemonCard CSS 작성

--- src/styledComponent/DexStyles/StyledPokemonCard.jsx
+++ src/styledComponent/DexStyles/StyledPokemonCard.jsx
@@ -2,29 +2,49 @@ import styled from "styled-components";
 
 export const StyledPokemonCard = styled.div`
   width: 150px;
-  height: 240px;
-  border: 1px solid red;
+  height: 250px;
+  margin: 10px;
 
+  background-color: #fff;
+  border-radius: 10px;
+
+  box-shadow: 3px 3px rgba(0, 0, 0, 0.2);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  font-size: 1.6em;
+  font-size: 1.3em;
 
   & img {
     width: 100%;
     height: 150px;
   }
 
-  & button {
-    border: none;
-    
+  & p {
+    margin: 5px 0;
   }
 
+  & button {
+    display: block;
+    width: 60px;
+    height: 25px;
 
+    color: #fff;
 
+    border: none;
+    border-radius: 5px;
+    background-color: #4fc753;
+  }
 
+  & button:hover {
+    cursor: pointer;
+    background-color: #48b34c;    
+  }
 
-
-`
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 2px 4px rgba(0, 0, 0, 0.1);
+    transition: 0.1s;
+  }
+`;
