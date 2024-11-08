fdd8c1fe3b05406ed9dddf3aab7eb765e9440d9d
Noon_sae <minseock7308@gmail.com>
2024-11-08 15:07:22 +0900

    📝MyPokemon 스타일링

--- src/styledComponent/DexStyles/StyledDashBoard/StyledMyPokemon.jsx
+++ src/styledComponent/DexStyles/StyledDashBoard/StyledMyPokemon.jsx
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
 export const StyledMyPokemon = styled.div`
-  display: block;
+  width: 150px;
+  height: 150px;
+
+  background-color: #fff;
+  border: 3px dashed #fca3c8; /* 투명한 대시 테두리 */  
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+
+  & img {    
+    width: 85px;
+    height: 85px;
+  }
 `;
-
-
