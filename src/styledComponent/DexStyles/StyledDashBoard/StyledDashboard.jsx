a6cb9b05b1f0fe38b0d46454042e9bf17aadc589
Noon_sae <minseock7308@gmail.com>
2024-11-11 16:34:43 +0900

    🔨여백조정 및 글씨 크기 조정

--- src/styledComponent/DexStyles/StyledDashBoard/StyledDashboard.jsx
+++ src/styledComponent/DexStyles/StyledDashBoard/StyledDashboard.jsx
@@ -5,12 +5,16 @@ export const StyledDashboard = styled.div`
   margin: 0 auto;
   padding: 30px 0;
   background-color: #fff3fb;
-
-  font-size: 2.1em;
+  
   text-align: center;
 
   & h2 {
-    margin-bottom: 30px;    
+    font-size: 3.4em;    
     color: #222;
-  }  
+  }
+
+  & p {
+    font-size: 2.1em;
+    margin: 20px 0;
+  }
 `;
