d3c47ec0303ab1c8731544c5ac30cac110b8191f
Noon_sae <minseock7308@gmail.com>
2024-11-13 23:40:23 +0900

    🔨Dashboard에서 필요없는 기능 제거

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -3,83 +3,54 @@ import { StyledMyPokemonWrap } from "../styledComponent/DexStyles/StyledDashBoar
 import { StyledMyPokemon } from "../styledComponent/DexStyles/StyledDashBoard/StyledMyPokemon";
 import { StyledPokemonCard } from "../styledComponent/DexStyles/StyledCardList/StyledPokemonCard";
 import pokeball from "../assets/images/pokeball.png";
-import { useNavigate } from "react-router-dom";
+
 import { StyledResetBtn } from "../styledComponent/DexStyles/StyledDashBoard/StyledResetBtn";
 
-const Dashboard = (
-  {      
-    selectedPokemon,    
-    handleDeletePokemon,
-    handleResetPokemon
-  }) => {
-
-  console.log(selectedPokemon);  
-
-  const navigate = useNavigate();
-  const goToDetailPage = () => {
-    navigate("/details");
-  }
-
+const Dashboard = ({
+  selectedPokemon,
+  handleDeletePokemon,
+  handleResetPokemon,
+}) => {
   return (
-    
-      <StyledDashboard>
-        <h2>포켓몬 도감</h2>
-        <p>포켓몬을 선택해주세요.</p>
-        <StyledResetBtn onClick={handleResetPokemon}>초기화버튼</StyledResetBtn>
-        <StyledMyPokemonWrap>
+    <StyledDashboard>
+      <h2>포켓몬 도감</h2>
+      <p>포켓몬을 선택해주세요.</p>
+      <StyledResetBtn onClick={handleResetPokemon}>초기화버튼</StyledResetBtn>
+      <StyledMyPokemonWrap>
+        {Array.from({ length: 6 }).map((_, index) => {
+          const currentPokemon = selectedPokemon[index];
 
-          {Array.from({ length: 6}).map((_, index) => {
-            const currentPokemon = selectedPokemon[index];
-            
-            return (
-              <>
+          return (
+            <>
               {currentPokemon ? (
                 <StyledPokemonCard key={currentPokemon.id}>
-                  <a>
-                    <img src={currentPokemon.img_url} alt={currentPokemon.korean_name} />
+                  <a className="selected">
+                    <img
+                      src={currentPokemon.img_url}
+                      alt={currentPokemon.korean_name}
+                    />
                   </a>
                   <h3>{currentPokemon.korean_name}</h3>
                   <span>No. {String(currentPokemon.id).padStart(3, "0")}</span>
-                  <button type="button" className="selected" id={currentPokemon.korean_name} onClick={handleDeletePokemon}>
+                  <button
+                    type="button"
+                    className="selected"
+                    id={currentPokemon.korean_name}
+                    onClick={handleDeletePokemon}
+                  >
                     삭제
                   </button>
                 </StyledPokemonCard>
-
-              ): (
+              ) : (
                 <StyledMyPokemon>
                   <img src={pokeball} alt="Pokeball" />
                 </StyledMyPokemon>
               )}
             </>
-            )})
-          }
-        </StyledMyPokemonWrap>
-      </StyledDashboard>
-
-            
-  
-
-
-
-          // <StyledMyPokemon>
-          //   <img src={pokeball} alt="Pokeball" />
-          // </StyledMyPokemon>
-          // <StyledMyPokemon>
-          //   <img src={pokeball} alt="Pokeball" />
-          // </StyledMyPokemon>
-          // <StyledMyPokemon>
-          //   <img src={pokeball} alt="Pokeball" />
-          // </StyledMyPokemon>
-          // <StyledMyPokemon>
-          //   <img src={pokeball} alt="Pokeball" />
-          // </StyledMyPokemon>
-          // <StyledMyPokemon>
-          //   <img src={pokeball} alt="Pokeball" />
-          // </StyledMyPokemon>
-
-
-        
-    
+          );
+        })}
+      </StyledMyPokemonWrap>
+    </StyledDashboard>
   );
 };
 
